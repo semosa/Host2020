0f106e716c80d69db424b33cac1bcd7503ba1a54
Sindre <se.mosand@stud.uis.no>
2020-09-21 14:17:12 +0200

    deleted

--- DAT250/flaskTest.py
+++ DAT250/flaskTest.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='about')
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
