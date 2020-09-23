5411a477dd4643eb1f284d5b627e1b7f365ce257
Sindre <se.mosand@stud.uis.no>
2020-09-23 09:14:37 +0200

    lagt til dummydata

--- DAT250/flaskTest.py
+++ DAT250/flaskTest.py
@@ -2,11 +2,29 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+kontoer = [
+    {
+        'type': 'brukskonto',
+        'kontonummer': '1234 01 00001',
+        'saldo': '0'
+    },
+    {
+        'type': 'sparekonto',
+        'kontonummer': '1234 01 00002',
+        'saldo': '0'
+    },
+    {
+        'type': 'BSU',
+        'kontonummer': '1234 01 00003',
+        'saldo': '0'
+    }
+]
+
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', kontoer=kontoer)
 
 
 @app.route('/about')
