7be749e5eb890bda64313457632a30c6553ea2e2
Sindre <se.mosand@stud.uis.no>
2020-09-25 13:42:37 +0200

    laget random kononummergenerator med kontrollutregning av siste siffer

--- DAT250/flaskTest.py
+++ DAT250/flaskTest.py
@@ -1,24 +1,26 @@
 from flask import Flask, render_template, url_for
+import random
 
 app = Flask(__name__)
 
+def kontonummer_generator():
+    forste_tall = "1234 "
+    andre_tall = "01 "
+    tredje_tall_random = random.randint(1000, 9999)
+    fjerde_tall_matte = abs(9 - (tredje_tall_random %11))
+    #print(fjerde_tall_matte)
+    fjerde_tall = str(fjerde_tall_matte)
+    tredje_tall = str(tredje_tall_random)
+    print(forste_tall + andre_tall + tredje_tall + fjerde_tall)
+    return forste_tall + andre_tall + tredje_tall + fjerde_tall
+
+
+kontonummer = kontonummer_generator()
 kontoer = [
-    {
-        'type': 'brukskonto',
-        'kontonummer': '1234 01 00001',
-        'saldo': '0'
-    },
-    {
-        'type': 'sparekonto',
-        'kontonummer': '1234 01 00002',
-        'saldo': '0'
-    },
-    {
-        'type': 'BSU',
-        'kontonummer': '1234 01 00003',
-        'saldo': '0'
-    }
-]
+    dict(type="Brukskonto", nummer=kontonummer, saldo=0),
+    dict(type="Sparekonto", nummer=kontonummer, saldo=0),
+    dict(type="BSU", nummer=kontonummer, saldo=0) 
+    ]
 
 
 @app.route('/')
@@ -33,3 +35,4 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    #kontonummer_generator()
